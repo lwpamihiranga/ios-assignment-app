45378660e74184fb8fa3b19466cdc24a4528913c
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-07-28 08:16:04 +0530

    my list view controller created

---
+++ iOS Assignment/MyListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MyListViewController.swift
+//  iOS Assignment
+//
+//  Created by Amith Mihiranga on 7/28/20.
+//  Copyright © 2020 Amith Mihiranga. All rights reserved.
+//
+
+import UIKit
+
+class MyListViewController: UIViewController {
+    
+    let listOfDetails = [MyDetail]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
