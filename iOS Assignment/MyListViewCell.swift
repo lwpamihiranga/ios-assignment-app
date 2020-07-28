8b3d6b8bd9b00a5d974675eda1564714e56b57af
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-07-28 08:16:24 +0530

    my list view cell created

---
+++ iOS Assignment/MyListViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyListViewCell.swift
+//  iOS Assignment
+//
+//  Created by Amith Mihiranga on 7/28/20.
+//  Copyright © 2020 Amith Mihiranga. All rights reserved.
+//
+
+import UIKit
+
+class MyListViewCell: UITableViewCell {
+ 
+    @IBOutlet weak var listViewCellImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+}
